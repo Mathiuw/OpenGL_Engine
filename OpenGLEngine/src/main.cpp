d7323d253ed325921af660109187555287b29db8
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2023-12-27 18:49:16 -0300

    Base

---
+++ OpenGLEngine/src/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() 
+{
+	//Initialize GLFW library
+	glfwInit();
+
+	//OpenGL Version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+//MAC OS Foward Compatibility
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Engine Test", NULL, NULL);
+
+	//Failed to create Window
+	if (window == NULL)
+	{
+		std::cout << "CREATE WINDOW FAILED" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//GLAD Failed to initialize
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress ) )
+	{
+		std::cout << "FAILED TO INITIALIZE GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//MAIN RENDER WHILE LOOP
+	while (!glfwWindowShouldClose(window) )
+	{
+		//process input
+		processInput(window);
+
+		//render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Generate and Send New Frame to Window
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
