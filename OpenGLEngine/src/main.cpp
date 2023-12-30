97ad18f2d8c7f1f067e06f3b518c24ecd6944ed0
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2023-12-30 00:59:02 -0300

    GLM, stb_images and Shaders
    Added GLM
    Added stb_images
    Displaying shapes(WIP)

--- OpenGLEngine/src/main.cpp
+++ OpenGLEngine/src/main.cpp
@@ -2,11 +2,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+std::string loadShaderSrc(const char* filename);
 
 int main() 
 {
+	std::cout << "Hello World" << std::endl;
+
+	int success;
+	char infolog[512];
+
 	//Initialize GLFW library
 	glfwInit();
 
@@ -45,6 +56,75 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	//SHADERS
+	//Compile Vertex Shader
+	unsigned int vextexShader;
+	vextexShader = glCreateShader(GL_VERTEX_SHADER);
+	std::string vertShaderSrc = loadShaderSrc("assets/vertex_core.glsl");
+	const GLchar* vertShader = vertShaderSrc.c_str();
+	glShaderSource(vextexShader, 1, &vertShader, NULL);
+	glCompileShader(vextexShader);
+
+	//Catch error
+	glGetShaderiv(vextexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vextexShader, 512, NULL, infolog);
+		std::cout << "ERROR WITH SHADER COMPILATION: " << std::endl << infolog << std::endl;
+	}
+
+	//Compile Fragment Shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	std::string fragShaderSrc = loadShaderSrc("assets/fragment_core.glsl");
+	const GLchar* fragShader = fragShaderSrc.c_str();
+	glShaderSource(fragmentShader, 1, &fragShader, NULL);
+	glCompileShader(fragmentShader);
+
+	//Catch error
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
+		std::cout << "ERROR WITH SHADER COMPILATION: " << std::endl << infolog << std::endl;
+	}
+
+	//Create Program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	//Linking shaders with program
+	glAttachShader(shaderProgram, vextexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	//Catch error
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
+		std::cout << "LINKING ERROR: " << std::endl << infolog << std::endl;
+	}
+
+	//Delete shaders
+	glDeleteShader(vextexShader);
+	glDeleteShader(fragmentShader);
+
+	//Vertex Array Object(VAO), Vertex Buffer Object(VBO)
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	//Bind VAO
+	glBindVertexArray(VAO);
+
+	//Bind VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+
 	//MAIN RENDER WHILE LOOP
 	while (!glfwWindowShouldClose(window) )
 	{
@@ -75,4 +155,28 @@ void processInput(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, true);
 	}
+}
+
+std::string loadShaderSrc(const char* filename)
+{
+	std::ifstream file;
+	std::stringstream buf;
+
+	std::string ret = "";
+
+	file.open(filename);
+
+	if (file.is_open())
+	{
+		buf << file.rdbuf();
+		ret = buf.str();
+	}
+	else
+	{
+		std::cout << "COULD NOT OPEN " << filename << std::endl;
+	}
+
+	file.close();
+
+	return ret;
 }
